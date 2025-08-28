e560e6a7fc51af32c9b8a51a358c81ed85afcd6c
Olalekan Rasaq <orasaq377@stu.ui.edu.ng>
2025-08-28 18:21:14 +0100

    Modify streamlit app codes

--- app/app.py
+++ app/app.py
@@ -25,17 +25,72 @@ st.set_page_config(page_title="Redshift Dashboard", layout="wide",
     }
 )
 
+# create database connection
+@st.cache_resource
+def get_connection(connection_string):
+    # Create the SQLAlchemy engine
+    engine = create_engine(connection_string)
+    return engine.connect()
+
+# query fact table
+@st.cache_data(ttl=3600)
+def load_fact_table():
+    conn = get_connection(connection_string)
+    query = text("SELECT * FROM public.fct_orders ORDER BY order_id DESC;")
+    result = conn.execute(query)
+    df = pd.DataFrame(result.fetchall(), columns=result.keys())
+    return df
+
+
 # Sidebar inputs for DB credentials
 st.sidebar.header("Olak Consult Enterprise")
 
-sel = st.sidebar.selectbox("Menu", ["Sales dashboard", "Customers Information"])
+sel = st.sidebar.selectbox("Menu", ["Sales dashboard", "Transaction table", "Customers Information"])
+
+if sel == "Sales dashboard":
+    col1, col2, col3 = st.columns(3, gap="medium")
+    with col1:
+        with st.container(border=True):
+            df = load_fact_table()
+            total_rev = float(df["amount"].sum())
+            st.metric(":moneybag: **Total Sales (USD)**", value=f"{total_rev:,.2f}")
+
+    with col2:
+        with st.container(border=True):
+            total_qty = float(df["quantity"].sum())
+            st.metric(":baggage_claim: **Total Quantity Ordered**", value=f"{total_qty:,.0f}")
+
+    with col3:
+        with st.container(border=True):
+            count_orders = len(df)
+            st.metric(":writing_hand: **No of Orders**", value=f"{count_orders:,.0f}")
+
+    chart_col1, chart_col2 = st.columns(2)
+    with chart_col1:
+        with st.container(vertical_alignment="center"):
+            st.caption("**Total Quantity sold per Item**")
+            item_qty = df.groupby(df.item_name)["quantity"].sum()
+            st.bar_chart(item_qty, x_label="Items", color="#8bfb9a", horizontal=True)
+
+    with chart_col2:
+        with st.container(vertical_alignment="center"):
+            st.caption("**Total revenue per Item**")
+            item_rev = df.groupby(df.item_name)["amount"].sum().astype(int)
+            st.bar_chart(item_rev, x_label="Items", color="#fb1779", horizontal=True)
+
+    df_col1, df_col2 = st.columns(2)
+    with df_col1:
+        with st.expander("View Items Category by Order quantities"):
+            st.dataframe(item_qty)
+    with df_col2:
+        with st.expander("View Items Category by Revenue"):
+            st.dataframe(item_rev)
+
 
 if sel == "Transaction table":
     try:
-        with engine.connect() as connection:
-            query = text("SELECT * FROM public.fct_orders LIMIT 10;")
-            result = connection.execute(query)
-            df = pd.DataFrame(result.fetchall(), columns=result.keys())
-            st.dataframe(df)
+        df = load_fact_table()
+        st.caption("**:chart: Most recent transactions**")
+        st.dataframe(df.head(20))
     except Exception as e:
         st.error(f"Error connecting to Redshift or executing query: {e}")
